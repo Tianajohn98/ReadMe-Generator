6773a2e4085be5725ba4187f7c59266db60fcd38
tiana <tianasjohnson98@gmail.com>
2021-11-28 19:29:09 -0500

    confirms and inputs

--- index.js
+++ index.js
@@ -5,23 +5,72 @@ const generateMarkdown = require('./src/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 //Questions to ask the user 
-const questions = [
+const promptUser = () => {
+    return inquirer.prompt([
     {
     
       type: "input" ,
       name: "github" ,
       message: "What is your Github username?",
-
+     
     },
 
     { 
-      
+      type: "input" ,
+      name: "title" ,
+      message: "what is the name of your Github project?" ,
+
+    },
+
+    {
+      type:"input" ,
+      name: "email" ,
+      message: "what is your email address?",
+
+    },
+
+    {
+      type: "input",
+      name: "description" ,
+      message: "Please write a description of your project",
+    },
+
+    {
+
+      type: "confirm",
+      name: "instal",
+      message: " Do you want to add any installation notes? ",
+
+     },
+
+     {
+      type: "input",
+      name: "installNotes",
+      message: "Please add the installation notes",
+       
+     },
+     { 
+       type: "confirm",
+       name: "usage",
+       message: "Do you want to add any usage information?",
+     },
+     {
+       type: "input",
+       name: "usageInput" ,
+       message: "Please add the usage information",
+
+     },
+
+     {  type: "confirm",
+        name: "contribution" ,
+        message: "Do you want to add any contributing notes?",
+       
 
 
+     }
 
 
-    }
-];
+]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -33,4 +82,3 @@ function init() {}
 init();
 
 
-console.log('hello node!');
